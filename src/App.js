e89dd3470bf58659bbec493ed6c6d307a032d95a
flow1981 <contact.f.stagliano@gmail.com>
2019-12-18 12:58:40 +0000

    changed displayed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello From React
+          Hello From React - React-CircleCI-AWS-Test
         </p>
         
       </header>
