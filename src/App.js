222f0cf82586212ab5bd893718c103cbd1554345
flow1981 <contact.f.stagliano@gmail.com>
2019-12-19 10:40:45 +0000

    added npm build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello From React - React-CircleCI-AWS-Test
+          Hello From React - React-CircleCI-AWS-Test - hello serg
         </p>
         
       </header>
